40266b4c4016b9dc69c3c2d0c0fcd27bf5a6f361
Louis Landelle <louis.landelle@epfl.ch>
2018-10-15 15:44:11 +0200

    Added implementations lsquaresGD, lsquaresSGD, lsquares, ridge, costs and compute_gradient

--- projects/project1/scripts/implementations.py
+++ projects/project1/scripts/implementations.py
@@ -1,26 +1,78 @@
 
 import numpy as np
 
+def calculate_mse(e):
+    """ Calculate the mse for vector e. """
+    return 1/2*np.mean(e**2)
+
+def compute_loss(y, tx, w):
+    """ Calculate the loss. """
+    e = y - tx.dot(w)
+    return calculate_mse(e)
+
+def compute_gradient(y, tx, w):
+    """ Compute the gradient. """
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """ Linear regression using gradient descent """
-    pass
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute loss, gradient
+        grad, err = compute_gradient(y, tx, w)
+        loss = calculate_mse(err)
+        # gradient w by descent update
+        w = w - gamma * grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return losses, ws
 
-def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
     """ Linear regression using stochastic gradient descent """
-    pass
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+            # compute a stochastic gradient and loss
+            grad, _ = compute_gradient(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+            # calculate loss
+            loss = compute_loss(y, tx, w)
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return losses, ws
 
 def least_squares(y, tx):
     """ Least squares regression using normal equations """
-    pass
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
 
-def ridge_regression(y, tx, lmbda):
+def ridge_regression(y, tx, lambda_):
     """ Ridge regression using normal equations """
-    pass
+    aI = lambda_ * np.identity(tx.shape[1])
+    a = tx.T.dot(tx) + aI
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     """ Logistic regression using gradient descent or SGD """
     pass
 
-def reg_logistic_regression(y, tx, lmbda, initial_w, max_iters, gamma):
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     """ Regularized logistic regression using gradient descent or SGD """
     pass
