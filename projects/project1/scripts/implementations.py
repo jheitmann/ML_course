4e4a8c7722438578568585b2b328a76ca6f63094
Louis Landelle <louis.landelle@epfl.ch>
2018-10-15 15:23:31 +0200

    Added implementations.py template

---
+++ projects/project1/scripts/implementations.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """ Linear regression using gradient descent """
+    pass
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    """ Linear regression using stochastic gradient descent """
+    pass
+
+def least_squares(y, tx):
+    """ Least squares regression using normal equations """
+    pass
+
+def ridge_regression(y, tx, lmbda):
+    """ Ridge regression using normal equations """
+    pass
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """ Logistic regression using gradient descent or SGD """
+    pass
+
+def reg_logistic_regression(y, tx, lmbda, initial_w, max_iters, gamma):
+    """ Regularized logistic regression using gradient descent or SGD """
+    pass
