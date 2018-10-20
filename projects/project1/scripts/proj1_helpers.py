dc85787dcb9d8576fb0e65f8e5f4f11e5097d5a5
Louis Landelle <louis.landelle@epfl.ch>
2018-10-20 11:11:08 +0200

    Thursday work - results folder and notebook

--- projects/project1/scripts/proj1_helpers.py
+++ projects/project1/scripts/proj1_helpers.py
@@ -23,7 +23,6 @@ def load_csv_data(data_path, step=None):
 
     return yb, input_data, ids
 
-
 def predict_labels(weights, data):
     """Generates class predictions given weights, and a test data matrix"""
     y_pred = np.dot(data, weights)
@@ -31,7 +30,6 @@ def predict_labels(weights, data):
     y_pred[np.where(y_pred > 0)] = 1
     
     return y_pred
-
 
 def create_csv_submission(ids, y_pred, name):
     """
--- projects/project1/scripts/train.py
+++ projects/project1/scripts/train.py
@@ -5,4 +5,5 @@ from implementations import least_squares_SGD
 
 yb, input_data, ids = load_csv_data('all/train.csv', step=50)
 losses, w = least_squares_SGD(yb, input_data, initial_w=np.zeros(30), batch_size=1, max_iters=30, gamma=0.5)
-print(losses, w)
+with open('output.txt', 'w') as fp:
+    print(yb, input_data, losses, w, file=fp)
