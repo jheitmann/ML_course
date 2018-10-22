b71b102c3a7a75b37bba1050be8039502d3b0528
Facenomore23 <32964159+Facenomore23@users.noreply.github.com>
2018-10-22 15:12:18 +0200

    Correction : On a besoin des y aussi

--- projects/project1/scripts/classif.py
+++ projects/project1/scripts/classif.py
@@ -10,14 +10,17 @@ import numpy as np
 """classify the data set in 3 differents classes"""
 
 def classif(X):
-    X0=X[(X[:,22]==0),:]
-    X1=X[(X[:,22]==1),:]
-    X2=X[(X[:,22]>1),:]
-    
+    X0=X[1][(X[1][:,22]==0),:]
+    X1=X[1][(X[1][:,22]==1),:]
+    X2=X[1][(X[1][:,22]>1),:]
+    Y0=X[0][(X[1][:,22]==0)]
+    Y1=X[0][(X[1][:,22]==1)]
+    Y2=X[0][(X[1][:,22]>1)]
+     
     X0=delcoll(X0)
     X1=delcoll(X1)
     X2=delcoll(X2)
-    return X0, X1, X2
+    return X0, Y0, X1, Y1, X2, Y2
 
 
 
