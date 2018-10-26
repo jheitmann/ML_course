73078414df9e485af341cab30bd45d1a8e96207e
jheitmann <julien.heitmann@epfl.ch>
2018-10-26 11:32:25 +0200

    Regression with feature engineering

--- projects/project1/scripts/proj1_helpers.py
+++ projects/project1/scripts/proj1_helpers.py
@@ -49,9 +49,9 @@ def create_csv_submission(ids, y_pred, name):
             
 def standardize(x):
     """Standardize the original data set."""
-    mean_x = np.mean(x)
+    mean_x = np.mean(x, axis = 0)
     x = x - mean_x
-    std_x = np.std(x)
+    std_x = np.std(x, axis = 0)
     x = x / std_x
     return x, mean_x, std_x
 
