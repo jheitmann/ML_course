b1667983284a81e2cf7a1d95a057efc8b0a6afaa
Louis Landelle <louis.landelle@epfl.ch>
2018-10-28 10:03:43 +0100

    Documented run.py. Did not use create_csv_submission because it only works on linux well (newline chars problem)

--- projects/project1/scripts/run.py
+++ projects/project1/scripts/run.py
@@ -1,31 +1,45 @@
 
 from benchmark_methods import model_output, compute_predictions, logistic_predictions
-from proj1_helpers import load_csv_data
+from proj1_helpers import load_csv_data, create_csv_submission
 import numpy as np
 import csv
+import sys
 
-test_data_path = 'all/test.csv'
-test = load_csv_data(test_data_path)
-_, test_data, test_ids, _ = test
+"""
+run.py is used to launch the application of weights on a test dataset and serialize the results.
 
-weights_path = 'all/weights.npy'
-weights = np.load(weights_path)
+It takes an optional argument 'logistic', which launches logistic_predictions if provided (otherwise, launches compute_predictions)
+"""
 
-clean_features_path = 'all/clean_features.npy'
-clean_features = np.load(clean_features_path)
+uses_logistic = True if len(sys.argv) > 1 else False
 
-parameters_path = 'all/parameters.npy'
-parameters = np.load(parameters_path)
+def load_npy(*npy_paths):
+    """
+    Returns numpy arrays serialized at npy_paths.
+    Args:
+        npy_paths : a sequence of serialized np.arrays files paths.
+    Returns:
+        Deserialized numpy arrays
+    """
+    return (np.load(p) for p in npy_paths)
 
+# Load the test dataset
+_, test_data, test_ids, _ = load_csv_data('all/test.csv')
+
+# Load the weights, feature masks and parameters (mean, std_dev)
+weights, clean_features, parameters = load_npy('all/weights.npy', 'all/clean_features.npy', 'all/parameters.npy')
+
+# Runs the weights against the test dataset
 out = model_output(test_data, weights, 22, clean_features, parameters)
 
-#TODO how to not manualy have to change? 
-pred = logistic_predictions(out)
-#pred = compute_predictions(out)
+# Converts the model output to discrete predictions
+pred = (compute_predictions if uses_logistic else logistic_predictions)(out)
 
+# Serializes the predictions to a csv fie
 with open('results.csv', 'w', newline='\n', encoding='utf-8') as fp:
     writer = csv.writer(fp)
-
     for i, id in enumerate(test_ids):
         if not i % 100000: print('Done:', i)
-        writer.writerow([id, 's' if pred[i] else 'b'])
+        writer.writerow([id, 1 if pred[i] else -1])
+
+#create_csv_submission(test_ids, pred, 'results.csv') Not cross-compatible
