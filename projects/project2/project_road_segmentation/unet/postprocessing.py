2fc4aee87a32009019bb64dbef653ee715cb4aac
jheitmann <julien.heitmann@epfl.ch>
2018-12-05 21:55:40 +0100

    Added baseline methods to processing scripts

--- projects/project2/project_road_segmentation/unet/postprocessing.py
+++ projects/project2/project_road_segmentation/unet/postprocessing.py
@@ -1,1 +1,99 @@
+import matplotlib.image as mpimg
 import numpy as np
+from PIL import Image
+
+PIXEL_DEPTH = 255
+IMG_PATCH_SIZE = 16
+
+def error_rate(predictions, labels):
+    """Return the error rate based on dense predictions and 1-hot labels."""
+    return 100.0 - (
+        100.0 *
+        np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) /
+        predictions.shape[0])
+
+# Write predictions from neural network to a file
+def write_predictions_to_file(predictions, labels, filename):
+    max_labels = np.argmax(labels, 1)
+    max_predictions = np.argmax(predictions, 1)
+    file = open(filename, "w")
+    n = predictions.shape[0]
+    for i in range(0, n):
+        file.write(max_labels(i) + ' ' + max_predictions(i))
+    file.close()
+
+# Print predictions from neural network
+def print_predictions(predictions, labels):
+    max_labels = np.argmax(labels, 1)
+    max_predictions = np.argmax(predictions, 1)
+    print (str(max_labels) + ' ' + str(max_predictions))
+
+def img_float_to_uint8(img):
+    rimg = img - np.min(img)
+    rimg = (rimg / np.max(rimg) * PIXEL_DEPTH).round().astype(np.uint8)
+    return rimg
+
+def concatenate_images(img, gt_img):
+    nChannels = len(gt_img.shape)
+    w = gt_img.shape[0]
+    h = gt_img.shape[1]
+    if nChannels == 3:
+        cimg = np.concatenate((img, gt_img), axis=1)
+    else:
+        gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
+        gt_img8 = img_float_to_uint8(gt_img)          
+        gt_img_3c[:,:,0] = gt_img8
+        gt_img_3c[:,:,1] = gt_img8
+        gt_img_3c[:,:,2] = gt_img8
+        img8 = img_float_to_uint8(img)
+        cimg = np.concatenate((img8, gt_img_3c), axis=1)
+    return cimg
+
+def make_img_overlay(img, predicted_img):
+    w = img.shape[0]
+    h = img.shape[1]
+    color_mask = np.zeros((w, h, 3), dtype=np.uint8)
+    color_mask[:,:,0] = predicted_img*PIXEL_DEPTH
+
+    img8 = img_float_to_uint8(img)
+    background = Image.fromarray(img8, 'RGB').convert("RGBA")
+    overlay = Image.fromarray(color_mask, 'RGB').convert("RGBA")
+    new_img = Image.blend(background, overlay, 0.2)
+    return new_img
+
+# Get prediction for given input image 
+def get_prediction(img):
+    """
+    data = np.asarray(img_crop(img, IMG_PATCH_SIZE, IMG_PATCH_SIZE))
+    data_node = tf.constant(data)
+    output = tf.nn.softmax(model(data_node))
+    output_prediction = s.run(output)
+    img_prediction = label_to_img(img.shape[0], img.shape[1], IMG_PATCH_SIZE, IMG_PATCH_SIZE, output_prediction)
+
+    return img_prediction
+    """
+    return []
+
+# Get a concatenation of the prediction and groundtruth for given input file
+def get_prediction_with_groundtruth(filename, image_idx):
+
+    imageid = "satImage_%.3d" % image_idx
+    image_filename = filename + imageid + ".png"
+    img = mpimg.imread(image_filename)
+
+    img_prediction = get_prediction(img)
+    cimg = concatenate_images(img, img_prediction)
+
+    return cimg
+
+# Get prediction overlaid on the original image for given input file
+def get_prediction_with_overlay(filename, image_idx):
+
+    imageid = "satImage_%.3d" % image_idx
+    image_filename = filename + imageid + ".png"
+    img = mpimg.imread(image_filename)
+
+    img_prediction = get_prediction(img)
+    oimg = make_img_overlay(img, img_prediction)
+
+    return oimg
