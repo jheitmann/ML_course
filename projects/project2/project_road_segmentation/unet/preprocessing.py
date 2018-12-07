3c3c694aedf1406d934f96bc1517a75dbd7e3336
louis landelle <louis.landelle@epfl.ch>
2018-12-07 23:03:22 +0100

    Implemented train.py aug part, small changes in non-aug part for compatibility, small changes and docstrings in preprocessing

--- projects/project2/project_road_segmentation/unet/preprocessing.py
+++ projects/project2/project_road_segmentation/unet/preprocessing.py
@@ -21,65 +21,103 @@ def img_crop(im, w, h):
             list_patches.append(im_patch)
     return list_patches
 
-def extract_data(filename, num_images, img_height, as_rgb):
-    """Extract the images into a 4D tensor [image index, y, x, channels].
+def extract_data(image_path, num_images, img_height, as_rgb, *, verbose=True):
+    """
+    Extract the images into a 4D tensor [image index, y, x, channels].
     Values are rescaled from [0, 255] down to [0, 1].
+    Args:
+        image_path: image folder path
+        num_images: size 0 of returned tensor, ammount of extracted images
+        img_height: resized image target width/height
+        as_rgb: flag set True when images need to be loaded as rgb
+        verbose: named flag set False to disable information printed by this method
+    Raises:
+        FileNotFoundError: if no image {filename}{id}.png is found for id in [1; num_images] 
+    Returns:
+        4D tensor [image index, y, x, channels]
     """
     imgs = []
     for i in range(1, num_images+1):
-        imageid = "_%.3d" % i
-        image_filename = filename + imageid + ".png"
-        if os.path.isfile(image_filename):
-            print ('Loading ' + image_filename)
-            img = cv2.imread(image_filename, as_rgb)
-            img = cv2.resize(img, dsize=(img_height, img_height), interpolation=cv2.INTER_AREA)
-            if not as_rgb:
-                img = img[..., np.newaxis]
-            img = img.astype('float32')
-            img /= PIXEL_DEPTH
-            imgs.append(img)
-        else:
-            print ('File ' + image_filename + ' does not exist')
+        imageid = "satImage_%.3d" % i
+        image_filename =  os.path.join(image_path, f"{imageid}.png")
+        if not os.path.isfile(image_filename):
+            raise FileNotFoundError(f"File {image_filename} does not exist.") 
+        if verbose:
+            print(f"Loading {image_filename}")
+        img = cv2.imread(image_filename, as_rgb)
+        img = cv2.resize(img, dsize=(img_height, img_height), interpolation=cv2.INTER_AREA)
+        if not as_rgb:
+            img = img[..., np.newaxis]
+        img = img.astype('float32')
+        img /= PIXEL_DEPTH
+        imgs.append(img)            
 
+    # Are those lines useful?
     num_images = len(imgs)
     img_width = imgs[0].shape[0]
     img_height = imgs[0].shape[1]
 
     return np.array(imgs)
-        
-# Assign a label to a patch v
-def value_to_class(v):
-    foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
+
+def value_to_class(v, foreground_threshold=0.25):
+    """
+    Assign a label to a patch v
+    Args:
+        v: patch np.array
+        foreground_threshold: percentage of pixels > 1 required to assign a foreground label to a patch
+    Returns:
+        one-hot vector of corresponding class
+    """
     df = np.sum(v)
     if df > foreground_threshold:
         return [0, 1]
     else:
         return [1, 0]
 
-# Extract label images
-def extract_labels(filename, num_images, img_height):
-    """Extract the labels into a 1-hot matrix [image index, label index]."""
+def extract_labels(label_path, num_images, img_height, *, verbose=True):
+    """
+    Extract the labels into a 1-hot matrix [image index, label index].
+    Args:
+        label_path: path to label folder
+        num_images: size 0 of returned tensor, ammount of extracted images
+        img_height: resized image target width/height
+        verbose: named flag set False to disable information printed by this method
+    Raises:
+        FileNotFoundError: if no image {filename}{id}.png is found for id in [1; num_images] 
+    Returns:
+        1-hot matrix [image index, label index]
+    """
     gt_imgs = []
     for i in range(1, num_images+1):
         imageid = "satImage_%.3d" % i
-        image_filename = filename + imageid + ".png"
-        if os.path.isfile(image_filename):
-            print ('Loading ' + image_filename)
-            labels = cv2.imread(image_filename, cv2.IMREAD_GRAYSCALE)
-            labels = cv2.resize(labels, (img_height, img_height))
-            labels = labels[..., np.newaxis]
-            labels = labels.astype('float32')
-            labels /= PIXEL_DEPTH
-            labels[labels >= 0.5] = 1
-            labels[labels < 0.5] = 0
-            gt_imgs.append(labels)
-        else:
-            print ('File ' + image_filename + ' does not exist')
+        image_filename = os.path.join(label_path, f"{imageid}.png")
+        if not os.path.isfile(image_filename):
+            raise FileNotFoundError(f"File {image_filename} does not exist.")
+        if verbose:
+            print (f"Loading {image_filename}")
+        labels = cv2.imread(image_filename, cv2.IMREAD_GRAYSCALE)
+        labels = cv2.resize(labels, (img_height, img_height))
+        labels = labels[..., np.newaxis]
+        labels = labels.astype('float32')
+        labels /= PIXEL_DEPTH
+        labels[labels >= 0.5] = 1
+        labels[labels < 0.5] = 0
+        gt_imgs.append(labels)
 
     return np.array(gt_imgs)
 
-# Convert array of labels to an image
 def label_to_img(imgwidth, imgheight, w, h, labels):
+    """
+    Convert array of labels to an image
+    Args:
+        imgwidth: width of returned image
+        imgheight: height of returned image
+        w: horizontal step
+        h: vertical step
+        labels: logit np.array
+    Returns:
+
+    """
     array_labels = np.zeros([imgwidth, imgheight])
     idx = 0
     for i in range(0,imgheight,h):
@@ -115,7 +153,7 @@ def listdirpaths(dirpath):
     """
     return [os.path.join(f) for f in os.listdir(dirpath)]
 
-def getTrainGenerator(batch_size,train_path,image_folder,label_folder,aug_dict,
+def get_train_generator(batch_size,train_path,image_folder,label_folder,aug_dict,
     image_color_mode="rgb",label_color_mode="grayscale",
     image_save_prefix="image",mask_save_prefix="mask",
     save_to_dir=None,target_size=(400,400),seed=1):
@@ -125,6 +163,7 @@ def getTrainGenerator(batch_size,train_path,image_folder,label_folder,aug_dict,
         image_folder: name of subdirectory in train_path containing images
         label_folder: name of subdirectory in train_path containing labels
         *_color_mode: color mode of *
+        *_save_prefix: save_prefix of flow_from_directory of * 
         save_to_dir: path of directory in which will be saved the generated pictures
         target_size: resizing size for both images and labels
         seed: rng seed
@@ -136,6 +175,10 @@ def getTrainGenerator(batch_size,train_path,image_folder,label_folder,aug_dict,
     image_datagen, label_datagen = ImageDataGenerator(**aug_dict), ImageDataGenerator(**aug_dict)
 
     # Makes flows
+    assert not image_folder.endswith(os.path.sep) and not image_folder.endswith('/'),\
+        f"The image path {image_folder} must NOT end with separator for some reason (ex: image/ -> image)"
+    assert not label_folder.endswith(os.path.sep) and not label_folder.endswith('/'),\
+        f"The label path {label_folder} must NOT end with separator for some reason (ex: label/ -> label)"
     image_generator = image_datagen.flow_from_directory(
         train_path,
         classes=[image_folder],
@@ -162,4 +205,4 @@ def getTrainGenerator(batch_size,train_path,image_folder,label_folder,aug_dict,
         for (image, label) in zip(image_generator, label_generator):
             yield convert_01(image, label)
 
-    return generator
+    return generator()
--- projects/project2/project_road_segmentation/unet/train.py
+++ projects/project2/project_road_segmentation/unet/train.py
@@ -1,15 +1,19 @@
 import argparse
 import numpy as np
 import skimage.io as io
+import os
 
 from keras.callbacks import ModelCheckpoint
 from model import unet
-from preprocessing import extract_data, extract_labels
+from preprocessing import extract_data, extract_labels, get_train_generator
 
-TRAINING_PATH = "data/train/"
-IMG_SUBFOLDER = "image/"
-GT_SUBFOLDER = "label/"
+TRAINING_PATH = "data/train"
+IMG_SUBFOLDER = "image"
+GT_SUBFOLDER = "label"
 N_TRAIN_IMAGES = 100
+
+TRAIN_IMG_PATH = os.path.join(TRAINING_PATH, IMG_SUBFOLDER)
+TRAIN_GT_PATH = os.path.join(TRAINING_PATH, GT_SUBFOLDER)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("img_height", type=int, choices=[256, 400],
@@ -34,25 +38,45 @@ print(f"Training on images of size {img_height}*{img_height} with {n_channels} i
 if (not args.augmented):
     print("Using raw data for training")
 
-    imgs = extract_data(TRAINING_PATH + IMG_SUBFOLDER + "satImage", N_TRAIN_IMAGES, img_height, args.rgb_images)
+    imgs = extract_data(TRAIN_IMG_PATH, N_TRAIN_IMAGES, img_height, args.rgb_images)
     #print(imgs.shape)
     #img0 = imgs[0]
     #print(img0.shape)
     #print(img0[:5,:5])
 
-    gt_imgs = extract_labels(TRAINING_PATH + GT_SUBFOLDER, N_TRAIN_IMAGES, img_height)
+    gt_imgs = extract_labels(TRAIN_GT_PATH, N_TRAIN_IMAGES, img_height)
     #print(gt_imgs.shape)
     #gt0 = gt_imgs[0]
     #print(gt0.shape)
     #print(gt0[:5,:5])
     #print(np.unique(gt0, return_counts=True))
 
-    input_size = (img_height,img_height,n_channels)
+    input_size = (img_height, img_height, n_channels)
     model = unet(input_size)
-    ckpt_file = f"results/unet_{"rgb" if args.rgb_images else "bw"}_{img_height}.hdf5"
+    ckpt_file = "results/unet_{}_{}.hdf5".format("rgb" if args.rgb_images else "bw", img_height)
     model_checkpoint = ModelCheckpoint(ckpt_file, monitor='loss', verbose=1, save_best_only=True)
     model.fit(x=imgs, y=gt_imgs, batch_size=batch_size, epochs=epochs, verbose=1,
                   validation_split=validation_split, shuffle=True, callbacks=[model_checkpoint]) # shuffle=False
     
 else:
     print("Using augmented dataset")
+
+    imgs = extract_data(TRAIN_IMG_PATH, N_TRAIN_IMAGES, img_height, args.rgb_images, verbose=False)
+    
+    gt_imgs = extract_labels(TRAIN_GT_PATH, N_TRAIN_IMAGES, img_height)
+
+    input_size = (img_height, img_height, n_channels)
+    model = unet(input_size)
+    ckpt_file = "results/unet_{}_{}_aug.hdf5".format("rgb" if args.rgb_images else "bw", img_height)
+    model_checkpoint = ModelCheckpoint(ckpt_file, monitor='loss', verbose=1, save_best_only=True)
+    data_gen_args = dict(rotation_range=0.2,
+                        width_shift_range=0.05,
+                        height_shift_range=0.05,
+                        shear_range=0.05,
+                        zoom_range=0.05,
+                        horizontal_flip=True,
+                        fill_mode='nearest')
+    save_to_dir = "data/aug"
+    steps_per_epoch = 10
+    train_generator = get_train_generator(batch_size, TRAINING_PATH, IMG_SUBFOLDER, GT_SUBFOLDER, data_gen_args, save_to_dir=save_to_dir, target_size=(img_height, img_height))
+    model.fit_generator(train_generator, steps_per_epoch=steps_per_epoch, epochs=epochs, verbose=1, callbacks=[model_checkpoint])
