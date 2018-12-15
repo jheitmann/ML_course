c9c7cb21a87dcee14e698844ecaae41a35522d28
louis landelle <louis.landelle@epfl.ch>
2018-12-15 13:36:57 +0100

    Broken grade_f1.py

---
+++ projects/project2/project_road_segmentation/unet/grade_f1.py
@@ -0,0 +1,16 @@
+
+import argparse
+from postprocessing import compute_trainset_f1
+import utest
+
+TEST_CSV_PATH = "results/output.csv"
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ckpt_path", type=str,
+                        help="path to ckpt file")
+    args = parser.parse_args()
+    utest.main(args.ckpt_path, t=True)
+    f1 = compute_trainset_f1(TEST_CSV_PATH)
+    print("f1 score :", f1)
+
