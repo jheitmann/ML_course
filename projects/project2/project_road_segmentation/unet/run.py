9e2a87e99ff992142c6b5d8987b56a3151a9f972
Louis Landelle <louis.landelle@epfl.ch>
2018-12-20 20:17:42 +0100

    Added run.py

---
+++ projects/project2/project_road_segmentation/unet/run.py
@@ -0,0 +1,36 @@
+import common
+import utrain
+import utest
+
+N_EPOCHS = 100
+N_STEPS_PER_EPOCH = 80
+
+dgargs = dict(    
+    rotation_range=90,
+    fill_mode='reflect',
+    horizontal_flip=True,
+    vertical_flip=True,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+)
+file_id = utrain.main(
+    img_height=256,
+    batch_size=2,
+    epochs=N_EPOCHS,
+    steps_per_epoch=N_STEPS_PER_EPOCH,
+    aug=True,
+    chosen_validation=True,
+    rgb=False,
+    pretrained_weights=None,
+    monitor="val_acc",
+    root_folder=".",
+    data_gen_args=dgargs
+)
+hdf5_path = os.path.join(common.RESULTS_PATH, file_id + ".hdf5")
+subm_path = utest.main(
+    ckpt_path=hdf5_path,
+    use_max=True,
+    four_split=True,
+    training=False,
+)
+print(f"Saved submission file at {subm_path}.")
