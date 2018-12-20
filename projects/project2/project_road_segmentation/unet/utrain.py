02f2dc7624284efc3d54eabdabd79a56fd61f3ae
Louis Landelle <louis.landelle@epfl.ch>
2018-12-20 15:15:22 +0100

    Cleaned utrain.py

--- projects/project2/project_road_segmentation/unet/utrain.py
+++ projects/project2/project_road_segmentation/unet/utrain.py
@@ -28,23 +28,29 @@ def main(img_height, batch_size, epochs, steps_per_epoch, aug, chosen_validation
     Raises:
         AssertionError: when encountering discrepancies in pretrained_weights/current_model rgb, img_height parameters
     """
+    # Prepares environment for the training (creates the folder structure needed for our code)
     prepare_train(os.getcwd() if not root_folder else root_folder, verbose=True)
 
+    # Computes the number of channels of the image and the validation_split ratio based on steps_per_epoch
     n_channels = 3 if rgb else 1
     validation_split = (common.N_TRAIN_IMAGES - steps_per_epoch) / float(common.N_TRAIN_IMAGES)
+    # Creates the model keras net for the chosen size/channels, and optionally loads pretrained weights
     input_size = (img_height,img_height,n_channels)
     model = unet(input_size, pretrained_weights=pretrained_weights)
 
-    # Create validation parameters dict. passed to fit(.)/fit_generator(.)
+    # Creates validation parameters dict. passed to fit(.)/fit_generator(.)
     validation_params = {"validation_data": None}
 
     if chosen_validation:
+        # Extracts the data of the chosen validation dataset (indices are in common.py)
         val_imgs = extract_data(common.SPLIT_VAL_IMG_PATH, common.N_SPLIT_VAL, img_height, rgb)
         val_gt_imgs = extract_labels(common.SPLIT_VAL_GT_PATH, common.N_SPLIT_VAL, img_height)
         validation_data = (val_imgs, val_gt_imgs)
         validation_params["validation_data"] = validation_data
+        # Monitors with val_acc by default, but is overridable via the model function argument
         monitor = monitor if monitor else "val_acc"
     else:
+        # Computes the number of validation steps based on the number of images and training steps per epoch
         validation_steps = common.N_TRAIN_IMAGES - steps_per_epoch
         if validation_split > 0:
             validation_params['validation_steps'] = validation_steps
@@ -54,6 +60,7 @@ def main(img_height, batch_size, epochs, steps_per_epoch, aug, chosen_validation
         else:
             monitor = monitor if monitor else ("val_acc" if validation_split > 0 else "acc")
 
+    # Checks if, when retraining from pretrained weights, they are compatible with the model shape
     if pretrained_weights:
         assert str(img_height) in pretrained_weights, "Wrong img_height pretrained weights"
         assert ("rgb" if rgb else "bw") in pretrained_weights, "Wrong color mode pretrained weights"
@@ -62,10 +69,12 @@ def main(img_height, batch_size, epochs, steps_per_epoch, aug, chosen_validation
     print("Using {} dataset {} chosen validation for training".format("raw" if not aug else "augmented", "with" if chosen_validation else "without"))
     print("Monitoring with", monitor)
     
+    # Creates Checkpoint callback to serialize our traiing weights
     file_id, model_checkpoint = get_checkpoint(img_height, rgb, monitor)
     # Create CSVLogger callback to retrieve the metrics history
     log_filename = os.path.join(common.LOGDIR, file_id + ".csv")
     if not aug:
+        # When not using dataset augmentation: extracts dataset, launches model.fit
         if chosen_validation:
             imgs = extract_data(common.SPLIT_TRAIN_IMG_PATH, common.N_SPLIT_TRAIN, img_height, rgb)
             gt_imgs = extract_labels(common.SPLIT_TRAIN_GT_PATH, common.N_SPLIT_TRAIN, img_height)
@@ -80,7 +89,7 @@ def main(img_height, batch_size, epochs, steps_per_epoch, aug, chosen_validation
     else:
         color_mode = "rgb" if rgb else "grayscale"
 
-        # save_to_dir=common.AUG_SAVE_PATH
+        # When using augmentation: fetches data generators, launches model.fit_generator
         if chosen_validation:
             train_generator, _ = get_generators(batch_size, common.SPLIT_TRAIN_PATH, common.IMG_SUBFOLDER, common.GT_SUBFOLDER, 
                                                     data_gen_args,  target_size=(img_height,img_height), color_mode=color_mode)   
@@ -96,6 +105,7 @@ def main(img_height, batch_size, epochs, steps_per_epoch, aug, chosen_validation
     return file_id
 
 if __name__=="__main__":
+    # Defines all parser arguments when launching the script directly in terminal
     parser = argparse.ArgumentParser()
     parser.add_argument("img_height", type=int,
                         help="image height in pixels")
@@ -119,20 +129,13 @@ if __name__=="__main__":
     parser.add_argument("--shift", type=float,
                         help="shift (x and y) augmentation for ImageDataGenerator (default:None)")
     args = parser.parse_args()
-    """
-    print(args.rotation, args.shift, args.zoom)
-    data_gen_args = dict(
-        rotation_range=args.rotation if args.rotation else common.DEFAULT_GEN_ARGS['rotation'],
-        width_shift_range=args.shift if args.shift else 0,
-        height_shift_range=args.shift if args.shift else 0,
-        zoom_range=args.zoom if args.zoom else 0
-    )
-    """
+
     data_gen_args = common.DEFAULT_GEN_ARGS
     if args.rotation: data_gen_args["rotation_range"] = args.rotation
     if args.shift: data_gen_args["width_shift_range"] = args.shift
     if args.shift: data_gen_args["height_shift_range"] = args.shift
     if args.zoom: data_gen_args["zoom_range"] = args.zoom
+
     main(args.img_height, args.batch_size, args.epochs, args.steps_per_epoch, args.augmented,
         args.chosen_validation, args.rgb_images, args.preweights, args.monitor,
         data_gen_args=data_gen_args)
